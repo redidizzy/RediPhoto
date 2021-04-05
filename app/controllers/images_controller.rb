3e941ec341fe38af78e2b4a43fb6626e4876dae1
Rediane Zemmouri <rediane.zemmouri@skaalab.com>
2021-04-05 13:28:00 +0100

    Update navigation menu

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,7 @@ class ImagesController < ApplicationController
 
   # GET /images or /images.json
   def index
-    @images = Image.all
+    @images = current_user.images
   end
 
   # GET /images/1 or /images/1.json
